33616b4c8d6f13d49825f49dd0c272d402e03334
Alex Sandro de Almeida <sanderghost@gmail.com>
2017-08-21 01:06:16 -0300

    Parei na aula 7
    Aula 7 Completa, recurso no, colocar no Janus.

--- index.php
+++ index.php
@@ -10,18 +10,55 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="includes/normalize.css">
-    <link rel="stylesheet" href="includes/font-awesome/">
+    <link rel="stylesheet" href="includes/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  
+  <div class="container relative">
+<!--   NAVBAR-->
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+          <a class="navbar-brand" href="#">ENYO</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="#">Features</a>
+              <a class="nav-item nav-link" href="#">Pricing</a>
+              <a class="nav-item nav-link" href="#">Disabled</a>
+            </div>
+          </div>
+    </nav>
+  <!--  END NAVBAR-->
+  
+    <h1>Free Psd Web Template</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat facilis minus perspiciatis perferendis accusamus.</p>
+    <button><i class="fa fa-download" aria-hidden="true"> Download</i></button>   
+    
+    
+    <img class="img-fluid banner-img" src="http://placehold.it/2000x5690" alt="">
+    
+    <h2>Just Download It</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat facilis minus perspiciatis perferendis accusamus.</p>
+    <ul>
+        <li><i class="fa fa-check-circle-o" aria-hidden="true"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</i></li>
+        <li><i class="fa fa-check-circle-o" aria-hidden="true"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</i></li>
+        <li><i class="fa fa-check-circle-o" aria-hidden="true"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</i></li>
+    </ul>
+    
+    </div><!--    END WRAPPED-->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="js/script.js"></script>
